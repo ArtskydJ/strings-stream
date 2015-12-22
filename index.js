b27ccd84e44889ee64d77b37ec96573d34ba7c10
Joseph Dykstra <josephdykstra@gmail.com>
2015-12-22 08:02:34 -0600

    Get tests passing on windows, add minLength option

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var through2 = require('through2')
+var xtend = require('xtend')
 
-function isStringy(val) { // a-zA-Z0-9\t //\n
-	return (val >= 32 && val <= 126) || val === 9 // || val === 10
+function isStringy(val) { // a-zA-Z0-9\t
+	return (val >= 32 && val <= 126) || val === 9
 }
 
-module.exports = function stringsStream() {
+module.exports = function stringsStream(options) {
+	var opts = xtend({ minLength: 4 }, options)
 	return new through2(function (chunk, enc, cb) {
 		var stringLike = false
 		var stringStart = 0
-		for (var i = 0; i < chunk.length; i++) {
+		for (var i = 0; i <= chunk.length; i++) {
 			var pieceIsStringLike = isStringy(chunk[i])
 			if (!stringLike && pieceIsStringLike) {
 				stringStart = i
-			} else if (stringLike && !pieceIsStringLike) {
-				this.push(chunk.slice(stringStart, i - 1))
-			} else if (stringLike && i === chunk.length - 1) {
-				this.push(chunk.slice(stringStart))
+			} else if (stringLike && !pieceIsStringLike && i - stringStart >= opts.minLength /*&& chunk[i] === 0*/) {
+				this.push(chunk.slice(stringStart, i) + '\r\n') // Maybe should be require('os').EOL
 			}
 			stringLike = pieceIsStringLike
 		}
--- test/test.js
+++ test/test.js
@@ -1,13 +1,27 @@
-﻿var test = require('tape')
+var test = require('tape')
 var compare = require('equal-streams')
 var fs = require('fs')
+var cp = require('child_process')
 var path = require('path')
 var stringsStream = require('../index.js')
 
+// If you do NOT have a `strings` executable
 test('basic functionality', function (t) {
 	var actualStream = fs.createReadStream(path.join(__dirname, 'What_Wondrous_Love_is_This.ppt'))
 	var expectStream = fs.createReadStream(path.join(__dirname, 'wwlit_strings.txt'))
 	compare(actualStream.pipe(stringsStream()), expectStream, function (err, equal) {
+		t.ifError(err)
+		t.ok(equal, 'the streams are equal')
+		t.end()
+	})
+})
+
+// If you have a `strings` executable
+test('spawn strings process', function (t) {
+	var actualStream = fs.createReadStream(path.join(__dirname, 'What_Wondrous_Love_is_This.ppt'))
+	var proc = cp.spawn('strings')
+	actualStream.pipe(proc.stdin)
+	compare(actualStream.pipe(stringsStream()), proc.stdout, function (err, equal) {
 		t.ifError(err)
 		t.ok(equal, 'the streams are equal')
 		t.end()
